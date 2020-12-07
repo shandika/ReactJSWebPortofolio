fd7f4d670d8f32c03cd04d2f36ab4ca350087e32
shandika <shandikaep@gmail.com>
2020-12-07 12:22:00 +0700

    initial commit added navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// components
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    (<div>
+      <MyNavbar />
+      Portofolio
+    </div>)
   );
 }
 
